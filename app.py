7ae21feb2543a20581e283888b84950d2f08952a
cgai <gaillard@brocelia.fr>
2022-07-01 10:54:04 +0200

    modification du requirement et code propre pour l'envoie de réponse

--- app.py
+++ app.py
@@ -38,23 +38,36 @@ def hello():
 @app.route('/tweet', methods=['POST'])
 
 def tweet():
-
+    response_data=[]
+    mondict= {}
     with open(os.path.join(app.root_path, 'tokenizer_glove'), 'rb') as handle :
         tokenizer = pickle.load(handle)
     model = load_model(os.path.join(app.root_path, 'my_model_Glove_LSTM'))
     request_data = request.get_json()
     text = request_data[0]['body']
     rs_text = wp.pipe_text(text)
-    prep_text = [gensim.utils.simple_preprocess(text) for text in rs_text]
+    print(rs_text)
+    #prep_text = [gensim.utils.simple_preprocess(text) for text in rs_text]
+
     k_sequence_len = 41
-    x_val = pad_sequences(tokenizer.texts_to_sequences(prep_text),
+    x_val = pad_sequences(tokenizer.texts_to_sequences([rs_text]),
                                                         maxlen= k_sequence_len,
                                                         padding='post')
-    print(x_val)
+
     y_pred_proba = model.predict(x_val)
     y_pred = np.where(y_pred_proba > 0.5, 1, 0)
-    print(y_pred)
-    return jsonify(request_data)
+    #print(y_pred[0][0])
+    if y_pred[0][0] == 1 :
+        sent = "Ce tweet est positif"
+    else:
+        sent = "Ce tweet est négatif"
+
+    mondict['pred'] = sent
+    mondict['proba'] =  str(y_pred_proba[0][0])
+    print(mondict)
+    response_data.append(mondict)
+    print(response_data)
+    return jsonify(response_data)
 
 
 
