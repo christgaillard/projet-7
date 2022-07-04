8eac8f3527847a9e8efc75ca8afd8b563417b32b
cgai <gaillard@brocelia.fr>
2022-07-04 07:52:30 +0200

    ajout des commentaires dans le code.

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import numpy as np
 from keras.saving.save import load_model
 from keras.utils import pad_sequences
 
-import webapp.functions as wp
+import webapp.functions as wp #import des fonctions
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory,jsonify
 
 
-app = Flask(__name__)
+app = Flask(__name__) #initialisation de Flask
 
-
+# page d'accueil de l'app
 @app.route('/')
 def index():
    print('Request for index page received')
@@ -22,25 +22,25 @@ def favicon():
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
-
+# definition du point d'entrée de l'appWeb
 @app.route('/tweet', methods=['POST'])
 
 def tweet():
     response_data=[]
     mondict= {}
     with open(os.path.join(app.root_path, 'tokenizer_glove'), 'rb') as handle :
-        tokenizer = pickle.load(handle)
-    model = load_model(os.path.join(app.root_path, 'my_model_Glove_LSTM'))
-    request_data = request.get_json()
-    text = request_data[0]['body']
-    rs_text = wp.pipe_text(text)
-    print(rs_text)
-    k_sequence_len = 41
+        tokenizer = pickle.load(handle) # chargement du Tokenizer pré entrainé
+    model = load_model(os.path.join(app.root_path, 'my_model_Glove_LSTM')) # chargement du modèle pre entrainé
+    request_data = request.get_json() # recupération des données envoyées en POST
+    text = request_data[0]['body'] # recupération du texte
+    rs_text = wp.pipe_text(text) # traitement du texte
+    k_sequence_len = 41 # definition de la longueur de séquence, doit correspondre au longueurs utilisées pour l'entrainement.
+    #créer notre séquence à partir du texte du tweet
     x_val = pad_sequences(tokenizer.texts_to_sequences([rs_text]),
                                                         maxlen= k_sequence_len,
                                                         padding='post')
 
-    y_pred_proba = model.predict(x_val)
+    y_pred_proba = model.predict(x_val) #prediction de notre tweet
     y_pred = np.where(y_pred_proba > 0.5, 1, 0)
     if y_pred[0][0] == 1 :
         sent = "Ce tweet est positif"
@@ -49,7 +49,7 @@ def tweet():
 
     mondict['pred'] = sent
     mondict['proba'] =  str(y_pred_proba[0][0])
-    response_data.append(mondict)
+    response_data.append(mondict) # renvoit de la réponse
     print(response_data)
     return jsonify(response_data)
 
--- webapp/functions.py
+++ webapp/functions.py
@@ -2,14 +2,22 @@ import re
 import nltk
 
 nltk.download('punkt')
-nltk.download('stopwords')
+nltk.download('stopwords') #import des stopwords anglais.
 from nltk.corpus import stopwords
 
 ps = nltk.PorterStemmer()
 stop_words = stopwords.words('english')   # on veille à charger les mots correspondants à la langue du document.
 
-
+'''
+Fonction pour le nettoyage du texte
+supprime les @, http, nombres etc...
+'''
 def tweet_transform(tweet):
+    '''
+
+    :param tweet: String
+    :return: String
+    '''
     text = re.sub(r'https?:\/\/.\S+', "", tweet)
     text = re.sub(r'#', '', text)
     text = re.sub(r'@([a-zA-z]*)', '', text)
@@ -17,24 +25,47 @@ def tweet_transform(tweet):
     text = re.sub(r'[^\w\s]', '', text)
     return text
 
-
+'''
+Découpe les phrases en listes de mots.
+'''
 def tokenize_words(text):
+    '''
+    :param text : string
+    :return: List[]
+    '''
     tokens = re.split('\W+', text)
     return tokens
 
 
-
+'''
+Supprime les mots récurrants, ayant un faible impacte sur le sens du texte.
+'''
 def remove_stopwords(texts):
+    '''
+    :param texts: List[]
+    :return: List[]
+    '''
     text = [word for word in texts if word not in stop_words]
     return text
 
-
+'''
+Traite chaque mots afin de le maitre à l'indicatif, et au singulier
+dans notre cas, fonctionne trés bien avec l'anglais.
+'''
 def stemming(texts):
+    '''
+
+    :param texts: list[]
+    :return: list[]
+    '''
     text = [ps.stem(word) for word in texts]
     trans_sentence = text
 
     return trans_sentence
-
+'''
+aplique en une fois tout les traitements de textes.
+renvois le texte prét pour le modèle
+'''
 def pipe_text(texts):
     texts = tweet_transform(texts)
     texts = tokenize_words(texts)
